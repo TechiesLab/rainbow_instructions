9dd43aeafccffaa397b1f84986cf5ebc7a8b72fc
Margotte <margo.tinawi@gmail.com>
2020-04-29 01:32:51 -0400

    Added project

--- projet/sketch.js
+++ projet/sketch.js
@@ -1,25 +1,22 @@
-let rayon;
-let origin;
-
 function setup() {
   createCanvas(400, 400);
-  rayon = 100;
-  origin = 0;
+  background(220);
+  noStroke();
+
+  let radius = 300;
+  let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", 220];
+
+  colors.forEach(color => {
+    drawArc(color, radius);
+    radius -= 40;
+  });
+
+  fill('black');
+  textSize(40);
+  text("Ça va bien aller!", 25, 300);
 }
 
-function draw() {
-  // background(220);
-  let c;
-  c = color('rgba(0,0,0,0)');
-  fill(c);
-  stroke('black');
-  line(0, origin, 400, origin);
-  line(origin,0, origin,400);
-  stroke('red');
-  circle(origin, origin, rayon);
-  circle(origin + rayon/2, origin, rayon);
-  circle(origin - rayon/2, origin, rayon);
-  circle(origin, origin + rayon/2, rayon);
-  circle(origin, origin - rayon/2, rayon);
-  origin+= 100;
+function drawArc(color, radius) {
+  fill(color);
+  arc(200, 200, radius, radius, -PI, 0);
 }
